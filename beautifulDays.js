b6287867cb4a6730014bafb0cb3fbc98e2175703
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-05 23:29:43 -0500

    Create beautifulDays.js
    https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem

---
+++ beautifulDays.js
@@ -0,0 +1,17 @@
+function beautifulDays(i, j, k) {
+  //https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+  let arrFor=''
+  let arrRev=''
+  let a=0
+  let b=0
+  let result=0
+  for (let p=i;p<=j;p++){
+      arrFor = [...p.toString()]
+      arrRev = arrFor.reverse()
+      a = parseInt(arrRev.join(''))
+      b=Math.abs(a-p)
+      result += b%k==0?1:0;
+  }//end for
+  return result
+  }
+}
