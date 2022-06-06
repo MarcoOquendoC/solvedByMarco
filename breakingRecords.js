7b8f72ad5f51117915b127bc31bc105dcfb52493
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-06 01:01:59 -0500

    Create breakingRecords.js
    https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem

---
+++ breakingRecords.js
@@ -0,0 +1,25 @@
+function breakingRecords(scores) {
+  //https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+  let highest=scores[0]
+  let lowest=scores[0]
+  let Lcount=0
+  let Hcount=0
+  let i=1
+  while(i<scores.length){
+  if(lowest<=scores[i] && scores[i]<=highest){
+  i++
+  } else if(scores[i]>highest){
+  highest=scores[i]
+  Hcount++
+  i++
+  } else if(scores[i]<lowest){
+  lowest=scores[i]
+  Lcount++
+  i++
+  }//end else
+  }//end while
+  console.log([Hcount,Lcount])
+  return ([Hcount,Lcount])
+  //breakingRecords([3,2,5,4,6])
+  }
+}
