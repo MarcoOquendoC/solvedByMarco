f3a970fb19770576a122ae48040e0dac2049eaa4
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-06 01:00:31 -0500

    create bonAppetit.js
    https://www.hackerrank.com/challenges/bon-appetit/problem

---
+++ bonAppetit.js
@@ -0,0 +1,23 @@
+function bonAppetit(bill,k, b) {
+  //https://www.hackerrank.com/challenges/bon-appetit/problem
+  let totalShared=0
+  let x=bill.length
+  let i=0
+  while(i<x){
+    if(i!=k){
+        totalShared += bill[0]
+        bill.shift()
+        i++
+    } else {
+        bill.shift()
+        i++
+    }//end else
+  }//end while
+  if(totalShared/2 === b){
+    console.log("Bon Appetit")
+  } else {
+    console.log((b - totalShared/2))
+  }//end else
+  //console.log(bonAppetit([3,10,2,9],1,12))
+  }
+}
