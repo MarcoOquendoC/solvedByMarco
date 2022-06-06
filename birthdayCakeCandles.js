6236598ab290c3cb5c876bb922bb03d26aecc5e1
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-06 00:20:12 -0500

    Create birthdayCakeCandles.js
    https://www.hackerrank.com/challenges/birthday-cake-candles/problem

---
+++ birthdayCakeCandles.js
@@ -0,0 +1,20 @@
+function birthdayCakeCandles(candles) {
+  //https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    let tallest=candles[0]
+    let count=1
+    let i=1
+    while(i<candles.length){
+    if(candles[i]<tallest){
+    i++
+    } else if(candles[i]>tallest){
+    tallest=candles[i]
+    i++
+    } else if(candles[i]==tallest){
+    count++
+    i++
+    }//end else
+    }//end while
+    console.log(count)
+    //birthdayCakeCandles([3,2,3,1,2,3,3,2,1])
+  }
+}
