a5d7edccffd490f651230b91148a06e6e1e7dae9
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-06 19:57:27 -0500

    Create chocolateFeast.js
    https://www.hackerrank.com/challenges/chocolate-feast/problem

---
+++ chocolateFeast.js
@@ -0,0 +1,12 @@
+function chocolateFeast(n=15, c=3, m=2) {
+    //https://www.hackerrank.com/challenges/chocolate-feast/problem
+    let chocolats = Math.floor(n/c)
+    let w = chocolats
+    let result=chocolats
+    while(w>=m){
+        chocolats = Math.floor(w/m)
+        w = w%m+chocolats
+        result += chocolats
+    }
+    return result
+  }
