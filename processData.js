0a20816fd5f365d7e1e2476a4855fb7677c398d4
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-06 19:40:00 -0500

    processData.js
    https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem

--- processData.js
+++ processData.js
@@ -1,22 +1,17 @@
 function processData(input) {
-  //https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
-    //Enter your code here
-    let arr = input.split('\n')
-    let dir = []
+    //https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+    let arr = input.split('\n').map(e=> e.split(' '))
+    const N = +arr[0]
+    let dir = {}
     let i=1
-    while(i<=arr[0]){
-        let elem = {}
-        let y=arr[i].split(' ')
-        elem.name = y[0]
-        elem.number = y[1]
-        dir.push(elem)
+    while(i <= N){
+        let [name, number]=arr[i]
+        dir[name]=number
         i++
     }
     while(i<arr.length){
-        let x=dir.find(e => e.name===arr[i])
-        console.log(x?x.name+'='+x.number:'Not found')
+        const [name,] = arr[i]
+        console.log(dir[name]? name+'='+dir[name]:'Not found')
         i++
     }
-    //console.log(arr)
-    //console.log(dir)
-} 
+}
