ace619d4a89f29c93b271965984e72df274d641d
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-10 23:44:28 -0500

    Create convertToF.js
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit

---
+++ convertToF.js
@@ -0,0 +1,9 @@
+convertToF(celsius) {
+  //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit
+  let fahrenheit;
+  fahrenheit = celsius*9/5+32;
+  return fahrenheit;
+  //F = C*9/5 +32
+  //C = 5/9 * (F - 32)
+  //console.log(convertToF(-40))
+  }
