aec1d5aa832ad5af6efc79bcd0e0093d8e76faa3
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 00:40:05 -0500

    Create extraLongFactorials.js
    https://www.hackerrank.com/challenges/extra-long-factorials/problem

---
+++ Solved/extraLongFactorials.js
@@ -0,0 +1,10 @@
+function extraLongFactorials(n) {
+  //https://www.hackerrank.com/challenges/extra-long-factorials/problem
+    let N=BigInt(n)
+    let result=BigInt(1)
+    for (let i=N; i>1 ;i--){
+        result *= i
+    }//end for
+    console.log(result.toString())
+  //console.log(extraLongFactorials(25))
+}
