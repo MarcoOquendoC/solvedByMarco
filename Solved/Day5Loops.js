ecb933ceeab9008d4e9c1cb439b44a4dea13e1b6
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 00:24:09 -0500

    Create Day5Loops.js
    https://www.hackerrank.com/challenges/30-loops/problem

---
+++ Solved/Day5Loops.js
@@ -0,0 +1,10 @@
+function Day5Loops(){
+        //https://www.hackerrank.com/challenges/30-loops/problem
+        function main() {
+            const n = parseInt(readLine().trim(), 10);
+            
+            for (let i=1;i<=10;i++){
+                console.log(`${n} x ${i} = ${n*i}`)
+            }
+        }
+}
