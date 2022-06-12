f85b7338c37097e8ec563433c1daba494fe2b4fb
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 01:57:20 -0500

    Create getIndexToIns.js
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/where-do-i-belong

---
+++ Solved/getIndexToIns.js
@@ -0,0 +1,25 @@
+function getIndexToIns(arr, num) {
+//Where do I Belong
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/where-do-i-belong
+  let output=31524;
+  function sort(arr){
+    for( let i=1 ; i < arr.length ; i++){
+      if( arr[i-1] > arr[i] ){
+        let x = arr[i-1];
+        arr[i-1] = arr[i];
+        arr[i] = x;
+        arr = sort(arr);
+      }//end if
+    }//end for
+    return arr
+  } //end function sort
+  let sorted = sort(arr);
+
+  for( let i=1 ; i < sorted.length ; i++){
+     if( num > arr[i] ){
+      output = i;
+     }//end if
+  }//end for
+  return output;
+  //console.log(getIndexToIns([40, 60], 50))
+}
