3dd531c6bb225aac1ff43832f45f4d023e8e4992
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 00:49:58 -0500

    Create getCount.js
    https://www.hackerrank.com/challenges/js10-count-objects/problem?isFullScreen=true

---
+++ Solved/getCount.js
@@ -0,0 +1,11 @@
+function getCount(objects) {
+  //https://www.hackerrank.com/challenges/js10-count-objects/problem?isFullScreen=true
+    let x=0
+    for(let i=0 ; i < objects.length ; i++){
+        if(objects[i].x === objects[i].y ){
+            x++
+        }//end if
+    }//end for
+    return x
+  //getCount( {x:12,y:12} ) 
+}
