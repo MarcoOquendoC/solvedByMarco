c40cce84145ef17adc50ead131c936eba4d5a550
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 02:29:54 -0500

    Create sumOddLengthSubarrays.js
    https://leetcode.com/problems/sum-of-all-odd-length-subarrays/

---
+++ Solved/sumOddLengthSubarrays.js
@@ -0,0 +1,11 @@
+function sumOddLengthSubarrays(arr){
+  //https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+    let sum=0
+    for (let j=1; j<=arr.length;j+=2){
+      for(let i= 0 ; i<arr.length-j+1 ; i++){
+        sum += (arr.slice(i,i+j)).reduce((acc,val) => acc+val)
+      }//end for
+    }//end for
+    return sum
+  //console.log(sumOddLengthSubarrays([1,4,2,5,3]))
+}
