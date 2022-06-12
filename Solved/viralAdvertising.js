e5f57ab2ed78632af789d9798e81d6bd8cf3dc8a
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 02:35:11 -0500

    Create viralAdvertising.js
    https://www.hackerrank.com/challenges/strange-advertising/problem

---
+++ Solved/viralAdvertising.js
@@ -0,0 +1,10 @@
+function viralAdvertising(n) {
+  //https://www.hackerrank.com/challenges/strange-advertising/problem
+  let accum=0n
+  let currentDayLikes=2n
+  for (let i=0n; i<n ; i++){
+      accum += currentDayLikes
+      currentDayLikes = currentDayLikes*3n/2n
+  }
+  return accum
+}
