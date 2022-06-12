a3ac95e09e5b311ceac7dcbbf8004f3aba07916a
Marco <104022866+MarcoOquendoC@users.noreply.github.com>
2022-06-12 01:59:32 -0500

    Create getSecondLargest.js
    https://www.hackerrank.com/challenges/js10-arrays/problem

---
+++ Solved/getSecondLargest.js
@@ -0,0 +1,11 @@
+function getSecondLargest(nums) {
+//Day 3: Arrays
+//https://www.hackerrank.com/challenges/js10-arrays/problem
+  let max = Math.max(...nums)
+  let second = 0
+  for(let i = 0 ; i<nums.length ; i++){
+    if (nums[i]<max && nums[i]>second)
+      second = nums[i]
+  }
+  return second
+}
