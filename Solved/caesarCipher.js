9abed286de6635b37072a97c295ad9d1aa610f5e
Marco <marcooquendoc@gmail.com>
2022-11-10 11:57:27 -0500

    Update caesarCipher
    Add freeCodeCamp solution to rot13 function

--- Solved/caesarCipher.js
+++ Solved/caesarCipher.js
@@ -4,7 +4,7 @@ function caesarCipher(s, k) {
     let alphabetLength=26
     k-= Math.floor(k/alphabetLength)*alphabetLength //if k>26
     //these are ascii codes
-    let aCode=65, zCode=90, ACode=97, ZCode=122, hiphenCode=45
+    let aCode=97, zCode=122, ACode=65, ZCode=90, hiphenCode=45
     for(let i=0;i<s.length;i++){
         let ascii = s.charCodeAt(i)
         if(aCode<=ascii && ascii<=zCode-k || ACode<=ascii && ascii<=ZCode-k){
@@ -17,4 +17,22 @@ function caesarCipher(s, k) {
     }//end for
     return arr.join('')
   }
+}
+
+function rot13(str) {
+  // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipher
+  let arr = str.split('')
+  let A=65, Z=90, a=97, z=122
+  let asciiArr = arr.map(e => {
+    return (
+      e.charCodeAt(0) >= A && e.charCodeAt(0) < A+13 ? e.charCodeAt(0) + 13 : 
+      e.charCodeAt(0) > Z-13 && e.charCodeAt(0) <= Z ? e.charCodeAt(0) - 13 :
+      e.charCodeAt(0) >= a && e.charCodeAt(0) < a+13 ? e.charCodeAt(0) + 13 : 
+      e.charCodeAt(0) > z-13 && e.charCodeAt(0) <= z ? e.charCodeAt(0) - 13 : 
+      e.charCodeAt(0)
+    )
+  });
+  let strArr = asciiArr.map(e => String.fromCharCode(e))
+  let ascii = String.fromCharCode(str.charCodeAt(0)-13)
+  return strArr.join('');
 }
