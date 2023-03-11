6bf6f824b245a7099ad190f66b59775876ff6014
Marco <marcooquendoc@gmail.com>
2023-03-11 14:25:52 -0500

    Create leetcode_merge_k_lists.rb

---
+++ Solved/leetcode_merge_k_lists.rb
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/description/
+def merge_k_lists(lists)
+    result = []
+    lists.each do |list|
+        _current = list
+        until _current == nil do
+            result.push(_current.val)
+            _current = _current.next
+        end
+    end
+    result.sort
+end
