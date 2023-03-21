f77b2474a84714d7462e3a863cdeda89e24640fd
Marco <marcooquendoc@gmail.com>
2023-03-21 15:27:47 -0500

    Create leetcode-remove-element.js

---
+++ Solved/leetcode-remove-element.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/remove-element/description/
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    while(nums.includes(val)) {
+        nums.splice(nums.indexOf(val), 1);
+    }
+    return nums.length
+};
