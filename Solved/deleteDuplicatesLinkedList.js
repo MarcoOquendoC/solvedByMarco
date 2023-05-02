5660bb4b38cd49f309e8b5e9e84de75040ae6694
Marco <marcooquendoc@gmail.com>
2023-05-02 15:59:05 -0500

    Create deleteDuplicatesLinkedList.js

---
+++ Solved/deleteDuplicatesLinkedList.js
@@ -0,0 +1,11 @@
+var deleteDuplicates = function(head) {
+  let current = head;
+  while(current) {
+    if(current.next && current.val === current.next.val) {
+      current.next = current.next.next
+      continue
+    }
+    current = current.next
+  }
+  return head
+};
