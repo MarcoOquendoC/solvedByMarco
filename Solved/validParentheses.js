d45039349ff6220ff35d32d7944d905ba5b4e40d
Marco <marcooquendoc@gmail.com>
2023-07-25 10:13:13 -0500

    Create validParentheses.js
    Solve leetcode challenge

---
+++ Solved/validParentheses.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+var isValid = function(s) {
+  const closing = (str) => {
+    if (str === '{') return '}';
+    if (str === '(') return ')';
+    if (str === '[') return ']';
+  }
+
+  const stack = [];
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === '[' || s[i] === '{' || s[i] === '(') {
+      stack.unshift(s[i]);
+    } else if(s[i] === closing(stack[0])) {
+      stack.shift();
+    } else if(s[i] === ']' || s[i] === '}' || s[i] === ')') {
+      return false;
+    }
+  }
+
+  if (stack.length === 0) return true;
+  return false;
+};
+
+let s = "(";
+console.log(isValid(s));
