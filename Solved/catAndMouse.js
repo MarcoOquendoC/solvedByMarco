5d56844764aff982ba0bea7eacc0ab0c5975ef05
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:30 -0500

    Correct Linters

--- Solved/catAndMouse.js
+++ Solved/catAndMouse.js
@@ -1,7 +1,7 @@
 function catAndMouse(x, y, z) {
-  //https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
-  return 
-  Math.abs(y-z)>Math.abs(x-z)? "Cat A"
-  :Math.abs(x-z)>Math.abs(y-z)? "Cat B"
-  :"Mouse C"
+  // https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+  return;
+  Math.abs(y - z) > Math.abs(x - z) ? 'Cat A'
+    : Math.abs(x - z) > Math.abs(y - z) ? 'Cat B'
+      : 'Mouse C';
 }
