e6942b41080ad223f1ad8581bb927ed8ddc6b51f
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:51 -0500

    Correct Linters

--- Solved/convertHTML.js
+++ Solved/convertHTML.js
@@ -1,18 +1,18 @@
 function convertHTML(str) {
-//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/convert-html-entities
-  let arr= [...str]
-  let ar=[]
-  arr.forEach(element=> ar.push(pairs(element)))
-  let st= ar.join('')
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/convert-html-entities
+  const arr = [...str];
+  const ar = [];
+  arr.forEach((element) => ar.push(pairs(element)));
+  const st = ar.join('');
   return st;
-  
-  function pairs(x){
-    if(x=="&")return "&amp;"
-    if(x=="<")return "&lt;"
-    if(x==">")return "&gt;"
-    if(x=='"')return "&quot;"
-    if(x=="'")return "&apos;"
-    return x
-  }//pairs(x)
-  //convertHTML("Dolce & Gabbana");
+
+  function pairs(x) {
+    if (x == '&') return '&amp;';
+    if (x == '<') return '&lt;';
+    if (x == '>') return '&gt;';
+    if (x == '"') return '&quot;';
+    if (x == "'") return '&apos;';
+    return x;
+  }// pairs(x)
+  // convertHTML("Dolce & Gabbana");
 }
