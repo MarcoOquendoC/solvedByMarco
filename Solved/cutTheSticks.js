2765a6f62f6dd4be8e39f69dfbbb42dbc9001d3c
Marco <marcooquendoc@gmail.com>
2023-08-03 16:00:12 -0500

    Correct Linters

--- Solved/cutTheSticks.js
+++ Solved/cutTheSticks.js
@@ -1,14 +1,14 @@
 function cutTheSticks(arr) {
-  //https://www.hackerrank.com/challenges/cut-the-sticks/problem
-  let result=[]
-  while(arr.length>1){
-      result.push(arr.length)
-      let min= Math.min(...arr)
-      arr.forEach((value,index) => arr.splice(index,1,value-min))
-      while (arr.includes(0)){
-          arr.splice(arr.indexOf(0),1)
-      }
+  // https://www.hackerrank.com/challenges/cut-the-sticks/problem
+  const result = [];
+  while (arr.length > 1) {
+    result.push(arr.length);
+    const min = Math.min(...arr);
+    arr.forEach((value, index) => arr.splice(index, 1, value - min));
+    while (arr.includes(0)) {
+      arr.splice(arr.indexOf(0), 1);
+    }
   }
-  if(arr.length!=0) result.push(arr.length)
-  return result
+  if (arr.length != 0) result.push(arr.length);
+  return result;
 }
