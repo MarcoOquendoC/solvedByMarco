8e100efe7e8919eee045c5d5152b1ecbdc713cd2
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:35 -0500

    Correct Linters

--- Solved/cavityMap.js
+++ Solved/cavityMap.js
@@ -1,16 +1,16 @@
 function cavityMap(grid) {
-    //https://www.hackerrank.com/challenges/cavity-map/problem
-    grid.forEach((value,index)=> grid.splice(index,1,[...value]))
-    for(let i=1;i<grid.length-1;i++){
-        for(let j=1;j<grid.length-1;j++){
-            if(parseInt(grid[i][j])>parseInt(grid[i][j-1]) 
-            && parseInt(grid[i][j])>parseInt(grid[i][j+1])
-            && parseInt(grid[i][j])>parseInt(grid[i-1][j]) 
-            && parseInt(grid[i][j])>parseInt(grid[i+1][j])){
-                grid[i][j]='X'
-            }//end if
-        }//end j loop
-    }//end i loop
-    grid.forEach((value,index)=> grid.splice(index,1,value.join('')))
-    return grid
+  // https://www.hackerrank.com/challenges/cavity-map/problem
+  grid.forEach((value, index) => grid.splice(index, 1, [...value]));
+  for (let i = 1; i < grid.length - 1; i++) {
+    for (let j = 1; j < grid.length - 1; j++) {
+      if (parseInt(grid[i][j]) > parseInt(grid[i][j - 1])
+            && parseInt(grid[i][j]) > parseInt(grid[i][j + 1])
+            && parseInt(grid[i][j]) > parseInt(grid[i - 1][j])
+            && parseInt(grid[i][j]) > parseInt(grid[i + 1][j])) {
+        grid[i][j] = 'X';
+      }// end if
+    }// end j loop
+  }// end i loop
+  grid.forEach((value, index) => grid.splice(index, 1, value.join('')));
+  return grid;
 }
