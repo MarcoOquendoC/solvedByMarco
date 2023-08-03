d665c4db830d83f3a6cce201f05d7044487f3a81
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:47 -0500

    Correct Linters

--- Solved/chunkyMonkey.js
+++ Solved/chunkyMonkey.js
@@ -2,26 +2,26 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
 
 function chunkArrayInGroups(arr, size) {
-  let quantity = Math.floor(arr.length / size);
-  let reminder = arr.length % size;
-  let result  = [];
+  const quantity = Math.floor(arr.length / size);
+  const reminder = arr.length % size;
+  const result = [];
 
-  if(reminder) {
-    let temp0 = []
-    for(let i = 1; i <= reminder; i++) {
+  if (reminder) {
+    const temp0 = [];
+    for (let i = 1; i <= reminder; i++) {
       temp0.unshift(arr.pop());
     }
-    result.unshift(temp0)
-  };
+    result.unshift(temp0);
+  }
 
-  for(let i = 0; i < quantity; i++) {
-    let temp = []
-    for(let j = 0; j < size; j++) {
+  for (let i = 0; i < quantity; i++) {
+    const temp = [];
+    for (let j = 0; j < size; j++) {
       temp.unshift(arr.pop());
     }
-    result.unshift(temp)
+    result.unshift(temp);
   }
   return result;
 }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2));
