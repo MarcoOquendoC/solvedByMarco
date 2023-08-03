37e772b9150bdc53d699671e947255cc5656a22c
Marco <marcooquendoc@gmail.com>
2023-08-03 16:00:18 -0500

    Correct Linters

--- Solved/Day6LetsReview.js
+++ Solved/Day6LetsReview.js
@@ -1,18 +1,18 @@
-function Day6LetsReview(){
-        //https://www.hackerrank.com/challenges/30-review-loop/problem
-        function processData(input) {
-            let arr= input.split('\n')
-            arr.shift()
-            let result=''
-            for (let j=0;j<arr.length;j++){
-                let even=[]
-                let odd=[]
-                for (let i=0; i<arr[j].length;i++){
-                    even += i%2==0? arr[j][i] : ''
-                    odd += i%2==0? '' : arr[j][i]
-                }
-                result+=(even+' '+odd+'\n')
-            }
-                console.log(result)
-        } 
+function Day6LetsReview() {
+  // https://www.hackerrank.com/challenges/30-review-loop/problem
+  function processData(input) {
+    const arr = input.split('\n');
+    arr.shift();
+    let result = '';
+    for (let j = 0; j < arr.length; j++) {
+      let even = [];
+      let odd = [];
+      for (let i = 0; i < arr[j].length; i++) {
+        even += i % 2 == 0 ? arr[j][i] : '';
+        odd += i % 2 == 0 ? '' : arr[j][i];
+      }
+      result += (`${even} ${odd}\n`);
+    }
+    console.log(result);
+  }
 }
