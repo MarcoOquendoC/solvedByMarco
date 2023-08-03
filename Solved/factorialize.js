5a303dd53076821089b3676bdd5af3a5f5e332c5
Marco <marcooquendoc@gmail.com>
2023-08-03 16:00:37 -0500

    Correct Linters

--- Solved/factorialize.js
+++ Solved/factorialize.js
@@ -1,9 +1,9 @@
 function factorialize(num) {
-  //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/factorialize-a-number
+  // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/factorialize-a-number
   let output = 1;
-  for(let i = num; i > 0; i--){
-      output *= i;
+  for (let i = num; i > 0; i--) {
+    output *= i;
   }
   return output;
-  //factorialize(3)
-  }
+  // factorialize(3)
+}
