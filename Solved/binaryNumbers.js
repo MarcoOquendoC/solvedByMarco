a5dd13a67602e008731c37cc5ba3eae84ffa2934
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:26 -0500

    Correct Linters

--- Solved/binaryNumbers.js
+++ Solved/binaryNumbers.js
@@ -1,11 +1,12 @@
 function main() {
-    // https://www.hackerrank.com/challenges/30-binary-numbers/problem
-    const n = parseInt(readLine().trim(), 10);
-    let N=n.toString(2)
-    let sum=0, result=0
-    for(let i=0 ; i<N.length ; i++){
-        sum=N[i]==1?sum+1:0
-        result=sum>result?sum:result
-    }
-    console.log(result)
+  // https://www.hackerrank.com/challenges/30-binary-numbers/problem
+  const n = parseInt(readLine().trim(), 10);
+  const N = n.toString(2);
+  let sum = 0; let
+    result = 0;
+  for (let i = 0; i < N.length; i++) {
+    sum = N[i] == 1 ? sum + 1 : 0;
+    result = sum > result ? sum : result;
+  }
+  console.log(result);
 }
