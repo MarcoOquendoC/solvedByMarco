739f23de88883379aa4d951712e0fb3968cf8431
Marco <marcooquendoc@gmail.com>
2023-08-03 16:00:08 -0500

    Correct Linters

--- Solved/countOddNumbersInAnInterval.js
+++ Solved/countOddNumbersInAnInterval.js
@@ -1,11 +1,11 @@
-function countOddNumbersInAnInterval(low,high){
-  //https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/submissions/
-    let x=0
-    if(high%2!=0 && low%2!=0){
-        x = (high-low+2)/2
-    }else{
-        x = Math.ceil((high-low)/2)
-    }//end else
-    return x
-  //console.log(countOddNumbersInAnInterval(3,7))
+function countOddNumbersInAnInterval(low, high) {
+  // https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/submissions/
+  let x = 0;
+  if (high % 2 != 0 && low % 2 != 0) {
+    x = (high - low + 2) / 2;
+  } else {
+    x = Math.ceil((high - low) / 2);
+  }// end else
+  return x;
+  // console.log(countOddNumbersInAnInterval(3,7))
 }
