ef99deb3768d3f20823f8699c2ce099b88b45579
Marco <marcooquendoc@gmail.com>
2023-08-03 15:59:16 -0500

    Correct Linters

--- Pramp/Getting a Different Number.js
+++ Pramp/Getting a Different Number.js
@@ -1,12 +1,11 @@
 // Getting a Different Number
 // https://www.pramp.com/challenge/aK6V5GVZ9MSPqvG1vwQp
 
-
 function getDifferentNumber(arr) {
   // [0,1,2,3,5] => 4
-  let set1 = new Set(arr);
-  for(let i = 0; i < arr.length; i++) {
-    if(!set1.has(i)) return i //arr[i] != i
+  const set1 = new Set(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (!set1.has(i)) return i; // arr[i] != i
   }
   return arr.length;
 }
