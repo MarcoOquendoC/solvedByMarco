6bd9b24ab8da74a7c932024f28cf464d05fb172c
Marco <marcooquendoc@gmail.com>
2023-08-03 16:00:04 -0500

    Correct Linters

--- Solved/countApplesAndOranges.js
+++ Solved/countApplesAndOranges.js
@@ -1,13 +1,13 @@
-function countApplesAndOranges(s,t,a,b,apples,oranges){
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // https://www.hackerrank.com/challenges/apple-and-orange/problem
-  function isElementIn(element){
-    if(s<=element && element<=t){return 1}else{return 0}
+  function isElementIn(element) {
+    if (s <= element && element <= t) { return 1; } return 0;
   }
-  let accumApples=0
-  let accumOranges=0
-  apples.forEach((element) => accumApples += isElementIn(element+a))
-  oranges.forEach((element) => accumOranges += isElementIn(element+b))
-  console.log(accumApples)
-  console.log(accumOranges)
-  //countApplesAndOranges(3,7,2,8,[1,1],[-1,-1])
+  let accumApples = 0;
+  let accumOranges = 0;
+  apples.forEach((element) => accumApples += isElementIn(element + a));
+  oranges.forEach((element) => accumOranges += isElementIn(element + b));
+  console.log(accumApples);
+  console.log(accumOranges);
+  // countApplesAndOranges(3,7,2,8,[1,1],[-1,-1])
 }
