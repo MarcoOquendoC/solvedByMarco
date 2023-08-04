428d084fd81723c9c76f1b191c877c5a94c9a464
Marco <marcooquendoc@gmail.com>
2023-08-04 13:10:32 -0500

    Fix linter errors

--- Solved/findDigits.js
+++ Solved/findDigits.js
@@ -1,12 +1,12 @@
 function findDigits(n) {
-  //https://www.hackerrank.com/challenges/find-digits/problem
-  let arr=[...n.toString()]
-  let accum=0
-  for (let i=0;i<arr.length;i++){
-      if(arr[i]!=0 && n%parseInt(arr[i])==0){
-          accum++
-      }//end if
-  }//end for
-  return accum
-  //findDigits(1012)
+  // https://www.hackerrank.com/challenges/find-digits/problem
+  const arr = [...n.toString()];
+  let accum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != 0 && n % parseInt(arr[i]) == 0) {
+      accum++;
+    }// end if
+  }// end for
+  return accum;
+  // findDigits(1012)
 }
