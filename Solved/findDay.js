8b8878aa0ead2d0e824a0c9a7816e31b30873bec
Marco <marcooquendoc@gmail.com>
2023-08-04 13:10:29 -0500

    Fix linter errors

--- Solved/findDay.js
+++ Solved/findDay.js
@@ -1,8 +1,8 @@
-function findDay(myDate='10/11/2009') {
+function findDay(myDate = '10/11/2009') {
   // https://www.hackerrank.com/contests/javascript-challenges/challenges
   // Return day for date myDate(MM/DD/YYYY)
   // Note that myDate contains the date in string format
-  let date=new Date(myDate)
-  let weekDays=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-  console.log(weekDays[date.getDay()])
+  const date = new Date(myDate);
+  const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  console.log(weekDays[date.getDay()]);
 }
