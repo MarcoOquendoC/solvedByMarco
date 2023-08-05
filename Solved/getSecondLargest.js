88596f0db3433dc30aae24b16d6d15ebea3e40c6
Marco <marcooquendoc@gmail.com>
2023-08-05 10:19:29 -0500

    Fix Linters

--- Solved/getSecondLargest.js
+++ Solved/getSecondLargest.js
@@ -1,11 +1,10 @@
 function getSecondLargest(nums) {
-//Day 3: Arrays
-//https://www.hackerrank.com/challenges/js10-arrays/problem
-  let max = Math.max(...nums)
-  let second = 0
-  for(let i = 0 ; i<nums.length ; i++){
-    if (nums[i]<max && nums[i]>second)
-      second = nums[i]
+// Day 3: Arrays
+// https://www.hackerrank.com/challenges/js10-arrays/problem
+  const max = Math.max(...nums);
+  let second = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < max && nums[i] > second) second = nums[i];
   }
-  return second
+  return second;
 }
