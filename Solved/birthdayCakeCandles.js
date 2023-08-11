cf5a01b07978a24fbea4bb6bd521dc9fc928f728
Marco <marcooquendoc@gmail.com>
2023-08-11 08:43:00 -0500

    Update birthdayCakeCandles.js

--- Solved/birthdayCakeCandles.js
+++ Solved/birthdayCakeCandles.js
@@ -1,20 +1,19 @@
 function birthdayCakeCandles(candles) {
-  //https://www.hackerrank.com/challenges/birthday-cake-candles/problem
-    let tallest=candles[0]
-    let count=1
-    let i=1
-    while(i<candles.length){
-    if(candles[i]<tallest){
-    i++
-    } else if(candles[i]>tallest){
-    tallest=candles[i]
-    i++
-    } else if(candles[i]==tallest){
-    count++
-    i++
-    }//end else
-    }//end while
-    console.log(count)
-    //birthdayCakeCandles([3,2,3,1,2,3,3,2,1])
-  }
+  // https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+  let tallest = candles[0];
+  let count = 1;
+  let i = 1
+  while(i<candles.length) {
+    if(candles[i] < tallest) {
+      i++;
+    } else if(candles[i] > tallest) {
+      tallest = candles[i]
+      i++;
+    } else if(candles[i] === tallest) {
+      count++;
+      i++;
+    }// end else
+  }// end while
+ console.log(count);
+ // birthdayCakeCandles([3,2,3,1,2,3,3,2,1])
 }
