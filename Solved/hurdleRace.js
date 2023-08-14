eb85161233f89cac00b7e67ead55f6d00090b4c1
Marco <marcooquendoc@gmail.com>
2023-08-14 05:02:09 -0500

    Update hurdleRace.js

--- Solved/hurdleRace.js
+++ Solved/hurdleRace.js
@@ -1,10 +1,10 @@
 function hurdleRace(k, height) {
-  //https://www.hackerrank.com/challenges/the-hurdle-race/problem
-  let max = Math.max(...height)
-  if(max>=k){
-    return max-k
+  // https://www.hackerrank.com/challenges/the-hurdle-race/problem
+  let max = Math.max(...height);
+  if(max >= k){
+    return max - k;
   } else {
-    return 0
-  }//end else
-  //console.log(hurdleRace(2,[6,5,4,7,1,3,2,1]))
+    return 0;
+  }// end else
+  // console.log(hurdleRace(2,[6,5,4,7,1,3,2,1]))
 }
