fcc2210c08aa487d69056240da7799a498c85c48
Marco <marcooquendoc@gmail.com>
2023-08-21 08:29:15 -0500

    Update binaryNumbers.js

--- Solved/binaryNumbers.js
+++ Solved/binaryNumbers.js
@@ -2,8 +2,8 @@ function main() {
   // https://www.hackerrank.com/challenges/30-binary-numbers/problem
   const n = parseInt(readLine().trim(), 10);
   const N = n.toString(2);
-  let sum = 0; let
-    result = 0;
+  let sum = 0;
+  let result = 0;
   for (let i = 0; i < N.length; i++) {
     sum = N[i] == 1 ? sum + 1 : 0;
     result = sum > result ? sum : result;
