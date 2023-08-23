3ed5af06050d314d98544ae1ba90dfaa2c771026
Marco <marcooquendoc@gmail.com>
2023-08-23 12:05:02 -0500

    Update Day5Loops.js

--- Solved/Day5Loops.js
+++ Solved/Day5Loops.js
@@ -3,7 +3,7 @@ function Day5Loops() {
   function main() {
     const n = parseInt(readLine().trim(), 10);
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 10; i += 1) {
       console.log(`${n} x ${i} = ${n * i}`);
     }
   }
