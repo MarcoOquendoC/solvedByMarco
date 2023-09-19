0c8780b0425366555bb1cf5dec9a8c3cbc05c4a3
Marco <marcooquendoc@gmail.com>
2023-09-18 22:55:17 -0500

    Update processData.js

--- Solved/processData.js
+++ Solved/processData.js
@@ -1,7 +1,7 @@
 function processData(input) {
     // https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
     let arr = input.split('\n').map(e=> e.split(' '));
-    const N = +arr[0]
+    const N = +arr[0];
     let dir = {};
     let i = 1;
     while(i <= N){
