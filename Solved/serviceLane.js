d754f135ef4d6839101a443b22c0ecd5c1a1d542
Marco <marcooquendoc@gmail.com>
2023-09-24 03:23:14 -0500

    Update serviceLane.js

--- Solved/serviceLane.js
+++ Solved/serviceLane.js
@@ -1,7 +1,7 @@
 function serviceLane(n, width, cases) {
-    //https://www.hackerrank.com/challenges/service-lane/problem
+    // https://www.hackerrank.com/challenges/service-lane/problem
     //"width" argument on line ~58
-    let arr=[]
-    cases.forEach(e=> arr.push(Math.min(...width.slice(e[0],e[1]+1))))
-    return arr
+    let arr = [];
+    cases.forEach(e=> arr.push(Math.min(...width.slice(e[0],e[1] + 1))));
+    return arr;
 }
