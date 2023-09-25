453daec8d334eebd1ecb505acd126a5b17316ade
Marco <marcooquendoc@gmail.com>
2023-09-25 10:32:51 -0500

    Update simpleArraySum.js

--- Solved/simpleArraySum.js
+++ Solved/simpleArraySum.js
@@ -1,12 +1,12 @@
 function simpleArraySum(ar) {
-//https://www.hackerrank.com/challenges/simple-array-sum/problem
-    //console.log(ar)
-    let x = 0
-    let i=0
+// https://www.hackerrank.com/challenges/simple-array-sum/problem
+    // console.log(ar)
+    let x = 0;
+    let i=0;
     while (i<ar.length){
-        x += ar[i]
-        i++
-    }//end while
-  return x
-  //simpleArraySum([1,2,3,4,5,6])
+        x += ar[i];
+        i++;
+    }
+  return x;
+  // simpleArraySum([1,2,3,4,5,6])
 }
