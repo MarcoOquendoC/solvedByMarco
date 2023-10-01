c11a970916814dfdba8a2d87cf253f6587a43021
Marco <marcooquendoc@gmail.com>
2023-10-01 13:24:45 -0500

    Complete linters fix balancedSums

--- Solved/balancedSums.js
+++ Solved/balancedSums.js
@@ -2,7 +2,7 @@ function balancedSums(arr) {
   // https://www.hackerrank.com/challenges/sherlock-and-array/problem
   let sum = arr.reduce((acc, val) => acc + val);
   let leftSum = 0;
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     sum -= arr[i];
     if (sum === leftSum) {
       return 'YES';
@@ -10,5 +10,6 @@ function balancedSums(arr) {
     leftSum += arr[i];
   }// end for
   return 'NO';
-  // console.log(balancedSums([3,2,1,6,5,4,9,3,2]))
 }
+
+balancedSums([3, 2, 1, 6, 5, 4, 9, 3, 2]);
