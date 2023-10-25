deb2e21d9721457486a48e38c319d1343976d26d
Marco <marcooquendoc@gmail.com>
2023-10-25 11:06:09 -0500

    Solved/convertHTML.js

--- Solved/convertHTML.js
+++ Solved/convertHTML.js
@@ -1,18 +1,19 @@
 function convertHTML(str) {
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/convert-html-entities
+  function pairs(x) {
+    if (x === '&') return '&amp;';
+    if (x === '<') return '&lt;';
+    if (x === '>') return '&gt;';
+    if (x === '"') return '&quot;';
+    if (x === "'") return '&apos;';
+    return x;
+  }
+
   const arr = [...str];
   const ar = [];
   arr.forEach((element) => ar.push(pairs(element)));
   const st = ar.join('');
   return st;
-
-  function pairs(x) {
-    if (x == '&') return '&amp;';
-    if (x == '<') return '&lt;';
-    if (x == '>') return '&gt;';
-    if (x == '"') return '&quot;';
-    if (x == "'") return '&apos;';
-    return x;
-  }// pairs(x)
-  // convertHTML("Dolce & Gabbana");
 }
+convertHTML('Dolce & Gabbana');
+console.log('smile!');
