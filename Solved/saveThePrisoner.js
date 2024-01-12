dcd23fe90ab052974c4e486a86afec4a98519313
Marco <marcooquendoc@gmail.com>
2024-01-11 23:01:13 -0500

    Update saveThePrisoner.js

--- Solved/saveThePrisoner.js
+++ Solved/saveThePrisoner.js
@@ -2,3 +2,4 @@ function saveThePrisoner(n, m, s) {
   // https://www.hackerrank.com/challenges/save-the-prisoner/problem
   return (m + s - 1) % n == 0 ? n : (m + s - 1 - Math.floor((m + s - 1) / n) * n);
 }
+console.log('smile');
